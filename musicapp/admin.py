60bebfe98cb79e749bb9aebd69963a85a935afeb
nystrome <nystrom.edwards@gmail.com>
2018-02-27 18:27:31 +0000

    Refactored search and helper fucntions

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from musicapp.models import Genre,Artist,Song,Comment,Rating,Album,PlayList
+from musicapp.models import *
 
 # Register your models here.
-admin.site.register(Genre)
 admin.site.register(Artist)
 admin.site.register(Comment)
 admin.site.register(Song)
--- musicapp/forms.py
+++ musicapp/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth.models import User
 
 
-
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     confirm_password = forms.CharField(widget=forms.PasswordInput())
@@ -24,6 +23,7 @@ class UserForm(forms.ModelForm):
         if email_count:
             raise forms.ValidationError('Email already exists')
         return email
+
 
 class UserEditForm(forms.ModelForm):
     current_password = forms.CharField(widget=forms.PasswordInput(), required=False)
@@ -50,4 +50,3 @@ class UserEditForm(forms.ModelForm):
         confirm_password = cleaned_data.get("confirm_password")
         if password != confirm_password:
             raise forms.ValidationError("Passwords do not match")
-
